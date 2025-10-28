a13df7b2f77fb48511451db89a50372ce1736335
Yeonguk Bak <yeonguk0517@gmail.com>
2025-10-28 22:20:50 +0900

    쇼핑몰 1 ~ 5 (2025. 10. 28. 과제)

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,11 @@
 const Product = ({ product }) => {
   return (
     <div className='product'>
-      {product.new && <span className='product__badge'>NEW</span>}
-      <img className='product__image' src={product.img} alt={product.title} />
-      <h4 className='product__title'>{product.title}</h4>
-      <p className='product__price'>₩ {product.price}</p>
+      {product?.new && <span className='product__badge'>NEW</span>}
+      <img className='product__image' src={product?.img} alt={product?.title} />
+      {product?.choice && <p className='product__choice'>Conscious choice</p>}
+      <h4 className='product__title'>{product?.title}</h4>
+      <p className='product__price'>₩ {product?.price}</p>
     </div>
   );
 };
